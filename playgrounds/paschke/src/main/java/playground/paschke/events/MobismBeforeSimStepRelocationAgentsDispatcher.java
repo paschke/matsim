7b4edbbe9adf62350623737dd94464072b0fec2c
paschke <stefan.paschke@gmail.com>
2015-11-15 18:54:10 +0100

    re-added some logging info

--- playgrounds/paschke/src/main/java/playground/paschke/events/MobismBeforeSimStepRelocationAgentsDispatcher.java
+++ playgrounds/paschke/src/main/java/playground/paschke/events/MobismBeforeSimStepRelocationAgentsDispatcher.java
@@ -139,6 +139,7 @@ public class MobismBeforeSimStepRelocationAgentsDispatcher implements MobsimBefo
 	private RelocationAgent getRelocationAgent(QSim qSim) {
 		for (RelocationAgent agent : this.relocationAgents.values()) {
 			if ((agent.getState() == State.ACTIVITY) && (agent.getRelocations().size() == 0)) {
+				log.info("RelocationAgent " + agent.getId() + " reused from the agent pool");
 				return agent;
 			}
 		}
