f893cb031d43b7ab6c7c44601c9a41f77229323e
paschke <stefan.paschke@gmail.com>
2016-07-05 21:30:05 +0200

    removed unused imports

--- contribs/carsharing/src/main/java/org/matsim/contrib/carsharing/control/listeners/FFEventsHandler.java
+++ contribs/carsharing/src/main/java/org/matsim/contrib/carsharing/control/listeners/FFEventsHandler.java
@@ -3,8 +3,6 @@ package org.matsim.contrib.carsharing.control.listeners;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-import org.apache.log4j.Logger;
-import org.jfree.util.Log;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.events.LinkLeaveEvent;
 import org.matsim.api.core.v01.events.PersonArrivalEvent;
@@ -19,7 +17,6 @@ import org.matsim.api.core.v01.events.handler.PersonLeavesVehicleEventHandler;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.network.Network;
 import org.matsim.api.core.v01.population.Person;
-import org.matsim.core.mobsim.qsim.agents.PersonDriverAgentImpl;
 import org.matsim.vehicles.Vehicle;
 
 public class FFEventsHandler implements PersonLeavesVehicleEventHandler, PersonEntersVehicleEventHandler, PersonArrivalEventHandler, PersonDepartureEventHandler, LinkLeaveEventHandler{
