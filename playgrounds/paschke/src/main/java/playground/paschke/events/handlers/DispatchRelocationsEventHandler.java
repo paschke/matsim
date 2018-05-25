e5fe28ee1b2d05e3d61c126849d65de94da1a123
paschke <stefan.paschke@gmail.com>
2018-05-25 14:57:46 +0200

    removed everything else from main playgrounds

--- playgrounds/paschke/src/main/java/playground/paschke/events/handlers/DispatchRelocationsEventHandler.java
+++
@@ -1,9 +0,0 @@
-package playground.paschke.events.handlers;
-
-import org.matsim.core.events.handler.EventHandler;
-
-import playground.paschke.events.DispatchRelocationsEvent;
-
-public interface DispatchRelocationsEventHandler extends EventHandler {
-	public void handleEvent(DispatchRelocationsEvent event);
-}
